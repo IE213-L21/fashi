92ae2c91ec87974a7bb7b5d018056ef734781a36
unknown <quachtuananh2016@gmail.com>
2021-06-13 12:41:54 +0700

    login logout

--- src/routes/account.js
+++ src/routes/account.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+var passport = require('passport');
 
 
 
 const accountController = require('../app/controllers/AccountController');
 
-router.get('/login', accountController.login);
-router.get('/register', accountController.register);
+
+router.get('/login',accountController.login)
+router.post('/login',passport.authenticate('local.login', { 
+    successRedirect: '/',
+    failureRedirect: '/account/login',
+    failureFlash: true }))
+router.get('/register',accountController.register)
+router.post('/register',passport.authenticate('local.register', { 
+    successRedirect: '/',
+    failureRedirect: '/account/register',
+    failureFlash: true }))
 
 
 module.exports = router ;
