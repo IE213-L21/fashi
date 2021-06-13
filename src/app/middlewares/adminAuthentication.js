2527404e01bd56437a0175d6b9dac80fa30547f9
Huu Phat <huuphat1908@gmail.com>
2021-06-13 22:46:45 +0700

    Phat

---
+++ src/app/middlewares/adminAuthentication.js
@@ -0,0 +1,6 @@
+module.exports = function adminAuthentication(){
+    // kiểm tra người dùng có role admin hay không?
+    // nếu ko thì redirect sang trang admin login
+    // lưu session
+    next();
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ const accountRouter = require('./account');
 const productRouter = require('./product');
 const adminRouter = require('./admin');
 
+const adminAuthentication = require('../app/middlewares/adminAuthentication');
+
 function route(app) {
     app.use('/blogs', blogRouter);
     app.use('/account', accountRouter);
     app.use('/product', productRouter);
-    app.use('/admin', adminRouter);
+    app.use('/admin', adminAuthentication, adminRouter);
     app.use('/', siteRouter);
 }
 
